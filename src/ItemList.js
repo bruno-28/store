9f8f91d9910c5dd2b34e2fd9d80adf9a1c804cb6
bruno-28 <bapn.14@gmail.com>
2021-03-10 18:31:17 +0100

    added container

--- src/ItemList.js
+++ src/ItemList.js
@@ -2,6 +2,7 @@ import React from "react";
 import { makeStyles } from "@material-ui/core/styles";
 import Grid from "@material-ui/core/Grid";
 import Item from "./Item";
+import { Container } from "@material-ui/core";
 
 const useStyles = makeStyles((theme) => ({
   root: {
@@ -14,17 +15,19 @@ export default function ItemList() {
 
   return (
     <div className={classes.root}>
-      <Grid container spacing={3}>
-        <Grid item xs={12} sm={4}>
-          <Item />
+      <Container>
+        <Grid container spacing={3}>
+          <Grid item xs={12} sm={4}>
+            <Item />
+          </Grid>
+          <Grid item xs={12} sm={4}>
+            <Item />
+          </Grid>
+          <Grid item xs={12} sm={4}>
+            <Item />
+          </Grid>
         </Grid>
-        <Grid item xs={12} sm={4}>
-          <Item />
-        </Grid>
-        <Grid item xs={12} sm={4}>
-          <Item />
-        </Grid>
-      </Grid>
+      </Container>
     </div>
   );
 }
